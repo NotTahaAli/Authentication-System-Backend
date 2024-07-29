a9a19c9623aa71a65c29784d8b2cb3839dc7e1fe
Muhammad Taha Ali <tahaali.6074@gmail.com>
2024-07-29 09:53:54 +0500

    Shifted SSL Certificates from src/keys to /keys

--- src/utils/createServer.util.ts
+++ src/utils/createServer.util.ts
@@ -9,13 +9,13 @@ export function createServer(app: Express) {
         console.log(`Http Server is running on port ${getHttpPort()}`);
     });
     if (getHttpsPort()) {
-        if (!fs.existsSync('./keys/key.pem') || !fs.existsSync('./keys/cert.pem') || !fs.existsSync('./keys/ca.pem')) {
+        if (!fs.existsSync('/keys/key.pem') || !fs.existsSync('/keys/cert.pem') || !fs.existsSync('/keys/ca.pem')) {
             console.log("Https Keys are not present. Https Server will not run.");
         } else {
             var options = {
-                key: fs.readFileSync('./keys/key.pem'),
-                cert: fs.readFileSync('./keys/cert.pem'),
-                ca: fs.readFileSync('./keys/ca.pem')
+                key: fs.readFileSync('/keys/key.pem'),
+                cert: fs.readFileSync('/keys/cert.pem'),
+                ca: fs.readFileSync('/keys/ca.pem')
             };
             try {
                 https.createServer(options, app).listen(getHttpsPort(), () => {
