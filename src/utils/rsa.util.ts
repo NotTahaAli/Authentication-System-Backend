0a7067e327d81a560a73a4a38dcfa766d6bc8447
Muhammad Taha Ali <tahaali.6074@gmail.com>
2024-07-29 12:55:19 +0500

    Converted RSA Module to use keys from Database instead of Local files.

--- src/utils/rsa.util.ts
+++ src/utils/rsa.util.ts
@@ -1,42 +1,33 @@
-import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
 import NodeRSA from "node-rsa"
+import keys from "../db/functions/keys";
 
 function generateKeys() {
     const key = new NodeRSA({ b: 512 });
     return { public_key: key.exportKey('pkcs8-public-pem'), private_key: key.exportKey('pkcs1-pem') }
 }
 
-export function getKeys() {
-    if (!existsSync('./keys')) {
-        mkdirSync("./keys")
-    }
-    if (!existsSync('./keys/public_key.pem') || !existsSync('./keys/private_key.pem')) {
+export async function getKeys() {
+    let public_key = await keys.getKey("public");
+    let private_key = public_key ? await keys.getKey("private") : null;
+    if (!public_key || !private_key) {
         const { public_key, private_key } = generateKeys();
-        writeFileSync("./keys/public_key.pem", public_key, {
-            encoding: "ascii",
-            flag: "w"
-        });
-        writeFileSync("./keys/private_key.pem", private_key, {
-            encoding: "ascii",
-            flag: "w"
-        });
+        await keys.setKey("public", public_key);
+        await keys.setKey("private", private_key);
         return { public_key, private_key };
     }
-    const public_key = readFileSync("./keys/public_key.pem", "ascii")
-    const private_key = readFileSync("./keys/private_key.pem", "ascii")
-    return { public_key, private_key }
+    return { public_key: public_key.key, private_key: private_key.key }
 }
 
-export function encrypt(data: Record<string | number, any>) {
+export async function encrypt(data: Record<string | number, any>) {
     const key = new NodeRSA();
-    key.importKey(getKeys().public_key, 'pkcs8-public-pem');
+    key.importKey((await getKeys()).public_key, 'pkcs8-public-pem');
     const encrypted = key.encrypt(JSON.stringify(data), 'base64');
     return encrypted;
 }
 
-export function decrypt<T = Record<string | number, any>>(encryptedData: string): T {
+export async function decrypt<T = Record<string | number, any>>(encryptedData: string): Promise<T> {
     const key = new NodeRSA();
-    key.importKey(getKeys().private_key, 'pkcs1-pem');
+    key.importKey((await getKeys()).private_key, 'pkcs1-pem');
     const decryptedString = key.decrypt(encryptedData, 'utf8');
     const decrypedObject = JSON.parse(decryptedString);
     return decrypedObject;
